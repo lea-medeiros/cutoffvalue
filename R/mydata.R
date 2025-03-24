e358af872bc66b9dced047210804fd5cc5e30a48
lea-medeiros <lmedeiros@uidaho.edu>
2025-03-24 10:58:21 -0700

    Package update

---
+++ R/mydata.R
@@ -0,0 +1,12 @@
+#' Log 11-keto testosterone values from age 2 male spring chinook
+#'
+#' A subset of data from a study investigating precocious maturation in hatchery-origin, age 2 male spring chinook in Cle Elum, WA
+#'
+#' @format ## `mydata`
+#' A list of 97 values:
+#' \describe{
+#'   \item{log 11-KT}
+#'   ...
+#' }
+#' @source <https://doi.org/10.1002/tafs.10097>
+"mydata"
