c49098eddb88ed11c45b06653dde9c4c1503ff25
lea-medeiros <lmedeiros@uidaho.edu>
2025-06-25 13:07:53 -0700

    Package update

---
+++ R/rawdata.R
@@ -0,0 +1,12 @@
+#' Juvenile male spring chinook plasma 11-KT levels
+#'
+#' A subset of plasma 11-KT values from age-2 juvenile male spring chinook collected to evaluate precocious maturation
+#'
+#' @format ## `rawdata`
+#' A data frame with 98 rows and 1 column:
+#' \describe{
+#'   \item{11-KT Values}
+#'   ...
+#' }
+#' @source <https://doi.org/10.1002/tafs.10097>
+"rawdata"
